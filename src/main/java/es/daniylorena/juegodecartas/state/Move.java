7d148e10a62f1586ba590e915495a7d629c9ad80
loren <lorenappff@gmail.com>
2025-07-04 20:10:10 +0200

    Constructores, getters, setters y comienzo de Move.validMove de la clase Move.java

--- src/main/java/es/daniylorena/juegodecartas/state/Move.java
+++ src/main/java/es/daniylorena/juegodecartas/state/Move.java
@@ -1,11 +1,38 @@
 package es.daniylorena.juegodecartas.state;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Move {
 
     private Set<Card> playedCards;
-
     private Player moveOwner;
+
+    public Move(Set<Card> playedCards, Player moveOwner) {
+        this.playedCards = playedCards;
+        this.moveOwner = moveOwner;
+    }
+
+    public Set<Card> getPlayedCards() {
+        return playedCards;
+    }
+
+    public void setPlayedCards(Set<Card> playedCards) {
+        this.playedCards = playedCards;
+    }
+
+    public Player getMoveOwner() {
+        return moveOwner;
+    }
+
+    public void setMoveOwner(Player moveOwner) {
+        this.moveOwner = moveOwner;
+    }
+
+    public boolean validMove(Set<Card> playedCards) {
+        boolean valid = true;
+        return valid;
+    }
 
 }
