194b08440ec563de96e07b4ef10521fda74e2da2
Daniel Rivera <daniel.rivera@alumnos.upm.es>
2025-07-30 21:55:35 +0200

    Primer test (funcional) ((sin mockito))

--- src/test/java/es/daniylorena/juegodecartas/state/DealerTest.java
+++ src/test/java/es/daniylorena/juegodecartas/state/DealerTest.java
@@ -2,16 +2,11 @@ package es.daniylorena.juegodecartas.state;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
 
 import java.util.List;
 
-
-@ExtendWith(MockitoExtension.class)
 public class DealerTest {
 
     @Test
@@ -34,11 +29,8 @@ public class DealerTest {
         // Comprueba que la baraja se ha vaciado
         assertTrue(deck.isEmpty());
 
-        // Comprueba que se ha llamado tantas veces al método draw() como cartas había en la baraja
-        verify(deck, times(deckSize)).draw();
-        int numCartas = 0;
-
         // Comprueba que el total de cartas se ha repartido entre los distintos jugadores
+        int numCartas = 0;
         for(var player : playerList){
             numCartas += player.getHandSize();
         }
